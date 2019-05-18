4a0664bc2cbe09e1196ed09d0ec7969fbabe6170
Biggamer11 <jetace64@gmail.com>
2019-05-17 20:38:59 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: jake
+ *
+ * Created on May 17, 2019, 8:20 PM
+ */
+
+#include <cstdlib>
+#include <string>
+#include <iostream>
+
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "this is a test";
+    return 0;
+}
+
